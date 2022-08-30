f1ac3de3e13a2d9fbe8c3cc5b05f0049f0428122
mavlonbek-sirojiddinov <mavlonbeksirojiddinov33@gmail.com>
2022-08-30 22:58:14 +0500

    add new birbalo

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@ var elSiteInput = document.querySelector("#curret-money");
 var elBtn = document.querySelector("#btn-js");
 var elselect = document.querySelector("#valuta-select")
 var elMessage = document.querySelector(".message")
+var elMessage2 = document.querySelector(".message")
 
 elSiteForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   var inputValue = parseFloat(elSiteInput.value, 10)
+  
+  elMessage2.textContent = ("PuL ");
 
-  elMessage.textContent = inputValue * elselect.value;
+  elMessage.textContent += inputValue *  elselect.value;
+
 });
 
 var button = document.querySelector(".white-btn");
